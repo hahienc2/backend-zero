94dd73d091bb063e0065f4e444dcffe0c05a0031
isaac <mbiz.isaac@gmail.com>
2023-09-24 20:18:26 +0700

    ádasdsa

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express();
-const port = 3000;
-const path = require('path');
+const express = require('express');// commonjs
+const path = require('path');//commonjs
+require('dotenv').config()
 
+// import express from 'express //es modules
+console.log("check env = ", process.env)
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 // config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// config static files
+app.use(express.static(path.join(__dirname,'public')));
+
 // khai bao route
 app.get('/abc', function (req, res) {
   res.send('Hello World ABC')
@@ -15,6 +23,6 @@ app.get('/hehe', function (req, res) {
   res.render('sample.ejs')
 })
 
-app.listen(port)
+app.listen(port, () => {console.log('port = ',port)})
 
 // Ctrl + C : tat 
